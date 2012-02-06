98a4fc5bb0318ea9bd4eedf4f840ab3448e9ea11
Shaun Laws <shaun@bluetent.com>
2012-02-06 16:54:11 -0700

    first commit

---
+++ js/btm_support_hp_slideshow_simple.js
@@ -0,0 +1,10 @@
+// Slideshow
+
+if (Drupal.jsEnabled) {
+  $(document).ready(function() {
+    $('#block-views-Slideshow-block_1 .view-content').cycle({
+      fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+    });
+  });
+}
+
